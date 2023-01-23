e571bf3072d1a1efb4743e4b1b69dd22c51fabf4
Kavin Valli <mail@kavin.me>
2023-01-24 00:20:59 +0530

    chore(examples): nextjs example

---
+++ examples/next-example/tailwind.config.js
@@ -0,0 +1,24 @@
+const { typewindTransforms } = require('typewind/transform');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    files: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+
+      // Or if using `src` directory:
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    transform: typewindTransforms,
+  },
+  theme: {
+    extend: {
+      colors: {
+        nextExamplePrimary: '#1FA5E9',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
