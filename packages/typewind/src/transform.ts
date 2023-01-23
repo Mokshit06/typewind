6d36825822c72c1d7c5eb480940aaba71186e52f
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-01-23 18:22:26 +0530

    feat: add tailwind transform
    Co-authored-by: Kavin Valli <mail@kavin.me>

---
+++ packages/typewind/src/transform.ts
@@ -0,0 +1,31 @@
+import * as babel from '@babel/core';
+import { TransformerFn } from 'tailwindcss/types/config';
+
+function transformBabel(ext: string, content: string) {
+  const config: babel.TransformOptions = {
+    filename: `typewind.${ext}`,
+    plugins: ['typewind/babel'],
+  };
+  if (ext === 'ts' || ext === 'tsx') {
+    config.presets = ['@babel/preset-typescript'];
+  }
+
+  if (ext === 'js' || ext === 'jsx') {
+    config.plugins?.push('@babel/plugin-syntax-jsx');
+  }
+
+  const res = babel.transformSync(content, config);
+
+  if (res?.code) {
+    return res?.code;
+  }
+
+  throw new Error('Failed to transform file');
+}
+
+export const typewindTransforms: Record<string, TransformerFn> = {
+  tsx: (content) => transformBabel('tsx', content),
+  ts: (content) => transformBabel('ts', content),
+  jsx: (content) => transformBabel('jsx', content),
+  js: (content) => transformBabel('js', content),
+};
