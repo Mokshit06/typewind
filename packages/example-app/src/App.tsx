27ce4b9734cd13c62cf12eb1a11e43c81b91bcf3
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-01-23 23:56:16 +0530

    chore: update example

--- packages/example-app/src/App.tsx
+++ packages/example-app/src/App.tsx
@@ -1,8 +1,9 @@
 import { tw } from 'typewind';
-import './index.css';
 
 export default function App() {
   return (
-    <h1 className={tw.text_red_800.dark(tw.text_red_400)}>tailwind + ts?</h1>
+    <button className={tw.flex.items_center.bg_blue_500.hover(tw.bg_blue_300)}>
+      Get started
+    </button>
   );
 }
