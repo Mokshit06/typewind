953c41b635fb2e3d7a689ee8cfaca1e55fd2b589
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-01-31 16:22:23 +0530

    test: naming convention

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -5,19 +5,15 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 describe('transformBabel', () => {
-  it('should work', () => {
-    expect(transformBabel).toBeDefined();
-  });
-
   const cwd = process.cwd();
   const fixturesDir = path.join(cwd, 'tests', 'fixtures');
   process.chdir(fixturesDir);
   const dirs = glob.sync('*', { cwd: fixturesDir });
-  test.each(dirs)('should work with %s', (dir) => {
+  test.each(dirs)('works with %s', dir => {
     const dirPath = path.join(fixturesDir, dir);
     process.chdir(dirPath);
     const files = glob.sync('*.{ts,tsx,js,jsx}', { cwd: dirPath });
-    files.forEach((file) => {
+    files.forEach(file => {
       const filePath = path.join(dirPath, file);
       const ext = path.extname(file).slice(1);
       const content = fs.readFileSync(filePath, 'utf-8');
