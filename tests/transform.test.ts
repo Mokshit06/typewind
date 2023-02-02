69af9d89a47f572a5385ada6936dc9c8f4cefafe
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-02-02 23:42:51 +0530

    fix: extract correct nodes in babel

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -4,22 +4,47 @@ import * as glob from 'glob';
 import * as path from 'path';
 import * as fs from 'fs';
 
+function setupTest(fixturesDir: string, dir: string) {
+  const dirPath = path.join(fixturesDir, dir);
+  process.chdir(dirPath);
+
+  const files = glob.sync('*.{ts,tsx,js,jsx}', { cwd: dirPath });
+
+  files.forEach(file => {
+    const filePath = path.join(dirPath, file);
+    const ext = path.extname(file).slice(1);
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const res = transformBabel(ext, content);
+    expect(res).toBeDefined();
+    expect(res).toMatchSnapshot();
+  });
+}
+
 describe('transformBabel', () => {
   const cwd = process.cwd();
   const fixturesDir = path.join(cwd, 'tests', 'fixtures');
-  process.chdir(fixturesDir);
-  const dirs = glob.sync('*', { cwd: fixturesDir });
-  test.each(dirs)('works with %s', dir => {
-    const dirPath = path.join(fixturesDir, dir);
-    process.chdir(dirPath);
-    const files = glob.sync('*.{ts,tsx,js,jsx}', { cwd: dirPath });
-    files.forEach(file => {
-      const filePath = path.join(dirPath, file);
-      const ext = path.extname(file).slice(1);
-      const content = fs.readFileSync(filePath, 'utf-8');
-      const res = transformBabel(ext, content);
-      expect(res).toBeDefined();
-      expect(res).toMatchSnapshot();
-    });
+
+  test('works with container-queries', () => {
+    setupTest(fixturesDir, 'container-queries');
+  });
+
+  test('works with arbitrary-values', () => {
+    setupTest(fixturesDir, 'arbitrary-values');
+  });
+
+  test('works with arbitrary-variants', () => {
+    setupTest(fixturesDir, 'arbitrary-variants');
+  });
+
+  test('works with modifiers', () => {
+    setupTest(fixturesDir, 'modifiers');
+  });
+
+  test('works with normal-usage', () => {
+    setupTest(fixturesDir, 'normal-usage');
+  });
+
+  test('works with transform', () => {
+    setupTest(fixturesDir, 'transform');
   });
 });
