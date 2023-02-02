63200043143cb47c19321ceea2a54440779ee6ba
Kavin Valli <mail@kavin.me>
2023-02-02 20:07:10 +0530

    chore(tests): add tw.important in modifiers fixture

--- tests/fixtures/modifiers/App.tsx
+++ tests/fixtures/modifiers/App.tsx
@@ -5,8 +5,9 @@ export default function Button() {
     <button
       className={tw.bg_blue_500
         .hover(tw.bg_blue_600)
-        .text_white.rounded.py_3.px_4.lu(tw.py_4.px_5)
-        .dark(tw.bg_sky_900.hover(tw.bg_sky_800))}
+        .text_white.rounded.py_3.px_4.md(tw.py_4.px_5)
+        .dark(tw.bg_sky_900.hover(tw.bg_sky_800))
+        .important(tw.text_red_500.hover(tw.text_red_600))}
     >
       Click Me
     </button>
