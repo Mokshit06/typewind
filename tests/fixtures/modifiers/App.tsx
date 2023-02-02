148c60cd6336fa3f79c4773f568d3c08164c478d
Kavin Valli <mail@kavin.me>
2023-02-02 20:20:42 +0530

    docs: add important modifier

--- tests/fixtures/modifiers/App.tsx
+++ tests/fixtures/modifiers/App.tsx
@@ -7,7 +7,8 @@ export default function Button() {
         .hover(tw.bg_blue_600)
         .text_white.rounded.py_3.px_4.md(tw.py_4.px_5)
         .dark(tw.bg_sky_900.hover(tw.bg_sky_800))
-        .important(tw.text_red_500.hover(tw.text_red_600))}
+        .important(tw.text_red_500)
+        .hover(tw.important(tw.text_red_500))}
     >
       Click Me
     </button>
