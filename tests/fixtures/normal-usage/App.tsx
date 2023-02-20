bbec6834919c90142e549903bb4fae3c7b906dc0
Kavin Valli <mail@kavin.me>
2023-02-20 19:39:37 +0530

    chore(tests): add test for opacity shorthand

--- tests/fixtures/normal-usage/App.tsx
+++ tests/fixtures/normal-usage/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
         tw.h_screen.w_full.bg_gray_800.flex.justify_center.items_center
       }
     >
-      <p className={tw.text_white}>Hello World!</p>
+      <p className={tw.text_blue_100$['25']}>Hello World!</p>
     </div>
   );
 }
