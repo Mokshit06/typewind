9ce470b8db91d77cd17f2ae198d59555660b5882
Kavin Valli <mail@kavin.me>
2023-02-20 19:42:03 +0530

    chore(tests): update test for new variant api

--- tests/fixtures/arbitrary-variants/App.tsx
+++ tests/fixtures/arbitrary-variants/App.tsx
@@ -2,6 +2,8 @@ import { tw } from 'typewind';
 
 export default function Button() {
   return (
-    <button className={tw['&:nth-child(3)'](tw.underline)}>Click Me</button>
+    <button className={tw.variant('&:nth-child(3)', tw.underline)}>
+      Click Me
+    </button>
   );
 }
