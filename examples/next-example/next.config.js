31cb2ee066796569cc2b0d1aceaa80bb8870a453
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-02-25 12:48:26 +0530

    chore: move next-example to swc

--- examples/next-example/next.config.js
+++ examples/next-example/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    swcPlugins: [['typewind/swc', {}]],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
