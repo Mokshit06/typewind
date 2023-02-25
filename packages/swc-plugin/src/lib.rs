c8919a81e69311b43c4aed8bfb31faf2690b2b96
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-02-25 12:13:47 +0530

    works and passes tests

--- packages/swc-plugin/src/lib.rs
+++ packages/swc-plugin/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 use swc_core::{
     common::DUMMY_SP,
@@ -9,31 +12,99 @@ use swc_core::{
     },
 };
 
+#[derive(Deserialize)]
+struct CandidateValueMap(HashMap<String, Vec<Vec<String>>>);
+
+impl Default for CandidateValueMap {
+    fn default() -> Self {
+        setup_rule_map()
+    }
+}
+
+fn fmt_to_tailwind(str: &str) -> String {
+    str.chars()
+        .map(|c| match c {
+            '$' => '@',
+            '_' => '-',
+            c => c,
+        })
+        .collect()
+}
+
 fn inner_most_is_tw(expr: &Expr) -> bool {
     match expr {
         Expr::Ident(ident) => ident.sym == *"tw",
         Expr::Member(member_expr) => inner_most_is_tw(&*member_expr.obj),
-        Expr::Call(call_expr) => inner_most_is_tw(&*call_expr.callee.as_expr().unwrap()),
+        Expr::Call(call_expr) => call_expr
+            .callee
+            .as_expr()
+            .and_then(|expr| Some(inner_most_is_tw(&**expr)))
+            .unwrap_or(false),
         _ => return false,
     }
 }
 
-// probably just be a visitor?
-fn analyse_expr(v: &mut TransformVisitor, wr: &mut String, e: &Expr) {
+fn analyse_expr(v: &mut TransformVisitor, wr: &mut Vec<String>, e: &Expr) {
     match e {
         Expr::Member(member_expr) => {
             let style = match &member_expr.prop {
-                MemberProp::Ident(ident) => ident.sym.as_ref(),
+                MemberProp::Ident(ident) => fmt_to_tailwind(ident.sym.as_ref()),
                 MemberProp::Computed(computed) => {
                     match computed.expr.as_lit().expect("Only literal values allowed") {
-                        Lit::Str(str) => str.value.as_ref(),
+                        Lit::Str(str) => str.value.to_string(),
                         _ => panic!("Only string literal values allowed"),
                     }
                 }
                 _ => panic!("Private values not allowed"),
             };
 
-            wr.push_str(&format!("{}{} ", v.get_prefix(), style));
+            if member_expr.prop.is_computed() {
+                v.style_queue.push_str(&style);
+            } else if !v.style_queue.is_empty() {
+                let maybe_arbitrary_val = v.style_queue.as_str();
+                let without_suffix = &style[..style.len() - 1];
+
+                let CandidateValueMap(rule_map) = &v.rule_map;
+
+                // @ means $ here as for opacity or named container queries etc.
+                // its being replaced to @ by the fmt_to_tailwind fn
+                if style.ends_with("@") {
+                    wr.push(format!(
+                        "{}{}/{}",
+                        v.get_prefix(),
+                        without_suffix,
+                        maybe_arbitrary_val
+                    ));
+                } else if let Some(rules) = rule_map.get(without_suffix) {
+                    let is_known_value = rules.iter().any(|rule| {
+                        if rule.contains(&maybe_arbitrary_val.to_string()) {
+                            true
+                        } else {
+                            false
+                        }
+                    });
+
+                    if is_known_value {
+                        wr.push(format!(
+                            "{}{}{}",
+                            v.get_prefix(),
+                            style,
+                            maybe_arbitrary_val
+                        ));
+                    } else {
+                        wr.push(format!(
+                            "{}{}[{}]",
+                            v.get_prefix(),
+                            style,
+                            maybe_arbitrary_val
+                        ));
+                    }
+                }
+
+                v.style_queue.clear()
+            } else {
+                wr.push(format!("{}{}", v.get_prefix(), style));
+            }
 
             analyse_expr(v, wr, &*member_expr.obj)
         }
@@ -48,12 +119,14 @@ fn analyse_expr(v: &mut TransformVisitor, wr: &mut String, e: &Expr) {
 
             analyse_expr(v, wr, &callee_member.obj);
 
-            let style = callee_member
-                .prop
-                .as_ident()
-                .expect("callee can only be identifier")
-                .sym
-                .as_ref();
+            let style = fmt_to_tailwind(
+                callee_member
+                    .prop
+                    .as_ident()
+                    .expect("callee can only be identifier")
+                    .sym
+                    .as_ref(),
+            );
 
             if style == "variant" {
                 let prefix = match &args
@@ -69,9 +142,15 @@ fn analyse_expr(v: &mut TransformVisitor, wr: &mut String, e: &Expr) {
 
                 v.cur_prefix.push(prefix);
 
-                analyse_expr(v, wr, &*args.get(1).expect("style not specified").expr)
+                analyse_expr(v, wr, &*args.get(1).expect("style not specified").expr);
+
+                v.cur_prefix.pop();
             } else {
-                v.cur_prefix.push(format!("{}:", style));
+                v.cur_prefix.push(if style == "important" {
+                    "!".to_string()
+                } else {
+                    format!("{}:", style)
+                });
 
                 analyse_expr(v, wr, &*args.get(0).expect("style not specified").expr);
 
@@ -86,6 +165,8 @@ fn analyse_expr(v: &mut TransformVisitor, wr: &mut String, e: &Expr) {
 pub struct TransformVisitor {
     cur_prefix: Vec<String>,
     inside_tw: bool,
+    style_queue: String,
+    rule_map: CandidateValueMap,
 }
 
 impl TransformVisitor {
@@ -100,15 +181,15 @@ impl TransformVisitor {
 
 impl VisitMut for TransformVisitor {
     fn visit_mut_expr(&mut self, e: &mut Expr) {
-        if inner_most_is_tw(&e) && !self.inside_tw {
+        if (e.is_call() || e.is_member()) && !self.inside_tw && inner_most_is_tw(&e) {
             self.inside_tw = true;
 
-            let mut wr = String::new();
+            let mut wr = Vec::<String>::new();
             analyse_expr(self, &mut wr, &e);
 
             *e = Expr::Lit(Lit::Str(Str {
                 span: DUMMY_SP,
-                value: wr.into(),
+                value: wr.join(" ").into(),
                 raw: None,
             }));
         } else {
@@ -122,12 +203,16 @@ pub fn process_transform(program: Program, _metadata: TransformPluginProgramMeta
     program.fold_with(&mut as_folder(TransformVisitor::new()))
 }
 
+fn setup_rule_map() -> CandidateValueMap {
+    serde_json::from_str::<CandidateValueMap>(include_str!("./value.json")).unwrap()
+}
+
 test!(
     Default::default(),
     |_| as_folder(TransformVisitor::new()),
     boo,
     // Input codes
-    r#"let style = tw.flex.items_center.md(tw.sm(tw.works).text_black)"#,
+    r#"let style = tw.flex.$lg(tw.bg_black$['20']).md(tw.important(tw.works).text_["18px"].text_["red-200"])"#,
     // Output codes after transformed with plugin
-    r#"let style = "items_center flex md:text_black md:sm:works ""#
+    r#"let style = "flex @lg:bg-black/20 md:text-red-200 md:text-[18px] md:!works""#
 );
